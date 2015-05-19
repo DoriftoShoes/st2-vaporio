bef54e94d45c0e71c1078e32156b1d0c8cf2b0d0
Patrick Hoolboom <patrick@stackstorm.com>
2015-05-19 12:01:33 -0700

    More bug fixes

--- actions/board_version.py
+++ actions/board_version.py
@@ -7,7 +7,7 @@ __all__ = [
 
 class BoardVersion(BaseVaporIOAction):
     def run(self, host, board_id, ssl=True):
-        endpoint = "version/%i" % board_id
+        endpoint = "version/%s" % board_id
         version = self._get_request(host=host, endpoint=endpoint, ssl=ssl)
 
         return version
--- actions/get_ports.py
+++ actions/get_ports.py
@@ -7,8 +7,8 @@ __all__ = [
 
 class GetPorts(BaseVaporIOAction):
     def run(self, host, board_id, ssl=True):
-        endpoint = "scan/%i" % board_id
+        endpoint = "scan/%s" % board_id
         board = self._get_request(host=host, endpoint=endpoint, ssl=ssl)
-        ports = board['ports']
+        ports = board['boards'][int(board_id) - 1]['ports']
 
         return ports

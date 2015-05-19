8cbcc6eb91bc869d6de1982c55496a17cc0b56eb
Patrick Hoolboom <patrick@stackstorm.com>
2015-05-19 10:56:29 -0700

    Disabled ssl, added port

--- actions/read_sensor.py
+++ actions/read_sensor.py
@@ -6,7 +6,7 @@ __all__ = [
 
 
 class ReadSensor(BaseVaporIOAction):
-    def run(self, host, sensor_type, board_id, port_id, ssl=True):
+    def run(self, host, sensor_type, board_id, port_id, ssl=False):
         endpoint = "read/%s/%i/%i" % (sensor_type, board_id, port_id)
         data = self._get_request(endpoint=endpoint)
 
